de2679ca40c82d6f7e6d755b0c19bbfa922f0b3f
rdaguas <aguas.rdv2001@gmail.com>
2024-12-20 00:46:38 +0000

    Taller18

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,47 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <Contador />
-  <Contador />
-
+  <Contador
+    encabezado=" Primero"
+    :valor="calcular(1, 3)"
+    :esVerdad="muestra1" :valor2="78"
+  />
+  <Contador
+    encabezado="Contador Segundo"
+    v-bind:valor="calcular(5, 3)"
+    :esVerdad="muestra2"
+  />
+  <button @:click="cambiar(1)">Cambiar Primero</button>
+  <button @:click="cambiar(2)">Cambiar Segundo</button>
 </template>
 
 <script>
 // Primer paso, realizar el import del componente "Contador"
-import Contador from './components/Contador.vue';
+import Contador from "./components/Contador.vue";
 export default {
   name: "App",
-// Segundo paso, registrar el componente en este caso "Contador"  
+  // Segundo paso, registrar el componente en este caso "Contador"
   components: {
-    Contador
-  }
+    Contador,
+  },
+  // no deberia progrmarse en el App
+  methods: {
+    calcular(a, b) {
+      return a + b;
+    },
+    cambiar(valor) {
+      if (valor === 1) {
+        this.muestra1 = !this.muestra1;
+      } else {
+        this.muestra2 = !this.muestra2;
+      }
+    },
+  },
+  data() {
+    return {
+      muestra1: true,
+      muestra2: true
+    };
+  },
 };
 </script>
 
--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,56 +1,91 @@
 <template>
-<h2>{{titulo}}</h2>
-<p>{{numero}} <sup>2</sup> = {{calcularCuadradoComputed}} </p>
-<p>{{numero}} <sup>2</sup> = {{calcularCuadradoComputed}} </p>
-<p>{{numero}} <sup>2</sup> = {{calcularCuadradoComputed}} </p>
-<p>{{numero}} <sup>2</sup> = {{calcularCuadradoComputed}} </p>
-<p>{{numero}} <sup>2</sup> = {{calcularCuadradoComputed}} </p>
-<div>
-    <button v-on:click="incrementar()">+1</button>
+  <h2>{{ encabezado }} : {{ valor2 }}</h2>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+  <p>{{ numero }} <sup>2</sup> = {{ calcularCuadradoComputed }}</p>
+
+  <div>
+    <button @:click="incrementar()">+1</button>
     <button v-on:click="decrementar()">-1</button>
-</div>
+  </div>
+  <div v-if="esVerdad">
+    <h1>Feliz Navidad</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            numero: 10,
-            titulo: 'Contador'
-        }
+  data() {
+    return {
+      numero: this.valor,
+      titulo: "Contador",
+    };
+  },
+  methods: {
+    calcularCuadrado() {
+      console.log("Método Entro a calcular");
+      return this.numero * this.numero;
     },
-    methods:{
-        calcularCuadrado(){
-            console.log('Método Entro a calcular');
-            return this.numero * this.numero;
-        },
-        incrementar(){
-            this.numero ++;
-        },
-        decrementar(){
-            this.numero --;;
-        }
+    incrementar() {
+      this.numero++;
     },
-    computed:{
-        calcularCuadradoComputed(){
-            console.log('Computed Entro a calcular');
-            return this.numero * this.numero;
-        }
-    }
+    decrementar() {
+      this.numero--;
+    },
+  },
+  computed: {
+    calcularCuadradoComputed() {
+      console.log("Computed Entro a calcular");
+      return this.numero * this.numero;
+    },
+  },
+  //Primera forma de declaracion de un props
+  //props: ['encabezado', 'valor'],
+
+  //Segunda forma de declaracion de un props
+  props: {
+    //validar encabezado, solo si no contiene la letra "a"
+    encabezado: {
+      type: String,
+      required: true,
+      validator(value) {
+        return !value.includes("a");
+      },
+    },
+    // encabezado: String,
+    valor: Number,
+    valor2: {
+      type: Number,
+      required: false,
+      default: 77,
+      validator(value){
+        //programo mi validacion bajo mi criterio y retorno true
+        //cuando es valido para mi y retorno false cuando no es valido
+        return value <=77;
+
+      }
+    },
+    esVerdad: {
+      type: Boolean,
+      required: true,
+    },
+    arreglo: Array,
+    fecha: Date,
+    objetoPersona: Object,
+  },
 };
 </script>
 
 <style>
-button{
-    background: #64B687;
-    border-radius: 5px;
-    border: 1px solid blue;
-    color: white;
-    cursor: pointer;
-    margin: 0px 10px;
-    padding: 12px 20px;
+button {
+  background: #64b687;
+  border-radius: 5px;
+  border: 1px solid blue;
+  color: white;
+  cursor: pointer;
+  margin: 0px 10px;
+  padding: 12px 20px;
 }
-button:hover{
-    background: #40895f;
+button:hover {
+  background: #40895f;
 }
 </style>
